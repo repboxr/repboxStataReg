6eeed157c6180c869333278a3803fd8ddc8e1cb8
skranz-auto <xsx50@uni-ulm.de>
2026-03-11 16:03:42 +0000

    Update from server

---
+++ R/inject_reg_stata.R
@@ -0,0 +1,27 @@
+# Will be called from repboxStata
+#
+# Inject regression specific information
+injection.reg = function(txt, lines=seq_along(txt),do, opts=rbs.opts()) {
+  restore.point("injection.reg")
+
+  repbox.dir = file.path(do$project_dir,"repbox/stata")
+  res.dir = file.path(repbox.dir,"tsv")
+
+  res.files = paste0(res.dir,"/",do$donum,"_",  lines,"_`repbox_local_cmd_count'",".dta")
+
+paste0('
+', end.injection(do$donum, lines, "RUNCMD",do),'
+* REGRESSION INJECTION START
+parmest, label saving("', res.files,'", replace)
+
+',post.injection(txt,lines,do=do, report.xtset=TRUE),'
+display "#~# INJECT REG_ERETURN ', do$donum,' ', lines,' `repbox_local_cmd_count\'"
+ereturn list
+display "#~# END INJECT REG_ERETURN ',do$donum,' ', lines,' `repbox_local_cmd_count\'"
+
+* REGRESSION INJECTION END
+')
+
+}
+
+
